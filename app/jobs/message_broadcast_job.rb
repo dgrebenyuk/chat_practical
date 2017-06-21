914313de305fd093ee0a34e0f0b670ebc99c7fa0
Artld <artld@expressmail.dk>
2017-06-21 19:21:52 +0300

    fixed sending message to Tg, view improvements

--- app/jobs/message_broadcast_job.rb
+++ app/jobs/message_broadcast_job.rb
@@ -6,8 +6,8 @@ class MessageBroadcastJob < ApplicationJob
   def perform(message)
     ActionCable.server.broadcast 'room_channel', message: render_message(message)
     if message.user_id.nil?
-      BotCommand::ForwardingToTg.new(nil, nil).send_to_group(render_message(message,'tg_message'))
-      # BotCommand::ForwardingToTg.new.send_to_group(render_message(message, 'tg_message'))
+      BotCommand::ToTg.new(nil, nil).send_to_group(render_message(message,'tg_message'))
+      # BotCommand::ToTg.new.send_to_group(render_message(message, 'tg_message'))
     end
   end
 
--- app/models/bot_message_dispatcher.rb
+++ app/models/bot_message_dispatcher.rb
@@ -9,8 +9,8 @@ class BotMessageDispatcher
 
   def process
     if chat_id > 0
-      BotCommand::ToTg.new(@user, @message).send_to_user('Hello! You should'\
-      'invite me to group, so I could read user messages and write messages'\
+      BotCommand::ToTg.new(@user, @message).send_to_user('Hello! You should '\
+      'invite me to group, so I could read user messages and write messages '\
       'sending by people outside Telegram. Speaking with me has no sence for now')
     else
       BotCommand::ToWeb.new(@user, @message).send_to_web
